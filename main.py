f075da64f4ef6c973ba57fd64c0bdf108dd5910b
qjalian <89934513+qjalian@users.noreply.github.com>
2023-12-10 17:39:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-def increasing_subsequences(sequence):
-    current_sequence = []
+def increasing_subsequences(sequence, current_sequence=None, result=None):
+    if current_sequence is None:
+        current_sequence = ()
+    if result is None:
+        result = []
 
-    for number in sequence:
-        if current_sequence and number >= current_sequence[-1]:
-            current_sequence.append(number)
-            if len(current_sequence) >= 2:
-                yield current_sequence[:]
-        else:
-            current_sequence = [number]
+    if not sequence:
+        return result
 
-while True:
-    user_input = input("Введите последовательность чисел, разделенных пробелами: ")
-    try:
-        user_numbers = [int(x) for x in user_input.split()]
-        user_numbers = [k for i, k in enumerate(user_numbers) if i == 0 or k != user_numbers[i - 1]]
-        if len(user_numbers) == 1:
-            print(user_numbers[0])
-        else:
-            previous_sequence = None
-            for subsequence in increasing_subsequences(user_numbers):
-                print(subsequence)
-        break
-    except ValueError:
-        print("Ошибка: Введите только целые числа, разделенные пробелами.")
+    number, *rest = sequence
+
+    if current_sequence and number >= current_sequence[-1]:
+        new_sequence = current_sequence + (number,)
+        if len(new_sequence) >= 2:
+            result.append(new_sequence)
+        increasing_subsequences(rest, new_sequence, result)
+    else:
+        increasing_subsequences(rest, (number,), result)
+
+    return result
+
+user_input = input("Введите последовательность чисел, разделенных пробелами: ")
+
+try:
+    user_numbers = [int(x) for x in user_input.split()]
+    user_numbers = [k for i, k in enumerate(user_numbers) if i == 0 or k != user_numbers[i - 1]]
+    if len(user_numbers) == 1:
+        print(user_numbers[0])
+    else:
+        sequences = increasing_subsequences(user_numbers)
+        for subsequence in sequences:
+            print(subsequence)
+
+except ValueError:
+    print("Ошибка: Введите только целые числа, разделенные пробелами.")
